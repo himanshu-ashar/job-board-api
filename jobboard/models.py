9d11c4355a45896c5d7dcd0b0e58de498c5a7e66
Himanshu <h.h.ashar@gmail.com>
2021-08-31 00:30:52 +0530

    conf settings, urls, api dir

--- jobboard/models.py
+++ jobboard/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class JobOffer(models.Model):
+    company_name = models.CharField(max_length=60)
+    company_email = models.EmailField()
+    job_title = models.CharField(max_length=60)
+    job_description = models.CharField(max_length=100)
+    salary = models.IntegerField()
+    city = models.CharField(max_length=60)
+    state = models.CharField(max_length=60)
+    created_at = models.DateTimeField(auto_now_add=True)
+    available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return "{} - {}".format(self.job_title, self.company_name)
