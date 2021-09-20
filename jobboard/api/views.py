ea03cab503868488dae757cd643750ef74175d6a
Himanshu <h.h.ashar@gmail.com>
2021-09-20 13:08:51 +0530

    init views

---
+++ jobboard/api/views.py
@@ -0,0 +1,6 @@
+from rest_framework.generics import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.response import Response
+
+from jobboard.models import JobOffer
